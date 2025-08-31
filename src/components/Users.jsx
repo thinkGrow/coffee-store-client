6d4c1e2fd25fb94c91a16ee20143391a841eece7
MaishaTahsin <rubaitreshad@gmail.com>
2025-08-31 19:36:21 +0600

    delete

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialUsers = useLoaderData();
 
   const [users, setUsers] = useState(initialUsers);
 
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              const remainingUsers = users.filter((user) => user._id != id);
+              setUsers(remainingUsers);
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div>
-      <h2>Users Count : {initialUsers.length}</h2>
+      <h2>Users Count : {users.length}</h2>
 
       <div className="overflow-x-auto">
         <table className="table">
@@ -18,6 +49,7 @@ const Users = () => {
               <th>No</th>
               <th>Name</th>
               <th>Phone</th>
+              <th>Email</th>
               <th>Favorite Color</th>
               <th></th>
             </tr>
@@ -45,9 +77,16 @@ const Users = () => {
                   }
                 </td>
                 <td>{user.phone}</td>
-                <td>Purple</td>
+                <td>{user.email}</td>
                 <th>
-                  <button className="btn btn-xs">details</button>
+                  <button className="btn btn-xs">View</button>
+                  <button className="btn btn-xs">Edit</button>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-xs"
+                  >
+                    Delete
+                  </button>
                 </th>
               </tr>
             ))}
